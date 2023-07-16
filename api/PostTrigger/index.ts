f8def1252ac4aa0e59601741b37d811931217d35
Yanai1005 <tatsuya871_1005@icloud.com>
2023-07-16 14:36:29 +0900

    fix:mega post

--- api/PostTrigger/index.ts
+++ api/PostTrigger/index.ts
@@ -1,17 +1,13 @@
 import * as fs from 'fs';
 import * as mysql from 'mysql2/promise';
 
-interface ThreadInfo {
-  thread_id: number;
-  thread_name: string;
-}
 interface MegaInfo {
     id: number;
     img_url: string;
-    thred_id: number;
+    thread_id: number;
     reply_id: number;
   }
-async function registerThread(threadInfo: ThreadInfo): Promise<void> {
+async function registerThread(MegaInfo: MegaInfo): Promise<void> {
   try {
     const config = {
       host: process.env["MYSQL_HOST"],
@@ -24,11 +20,11 @@ async function registerThread(threadInfo: ThreadInfo): Promise<void> {
 
     const conn = await mysql.createConnection(config);
     const [rows, fields] = await conn.execute(
-      'INSERT INTO thread (thread_id, thread_name) VALUES (?, ?)',
-      [threadInfo.thread_id, threadInfo.thread_name]
+      'INSERT INTO mega (id, img_url,reply_id,thread_id) VALUES (?, ?, ?, ?)',
+      [MegaInfo.id, MegaInfo.img_url,MegaInfo.reply_id,MegaInfo.thread_id]
     );
 
-    console.log('threadテーブルにデータを登録しました。');
+    console.log('megaテーブルにデータを登録しました。');
   } catch (error) {
     // エラーが発生した場合の処理
     console.log('データベースへの登録中にエラーが発生しました:', error.message);
@@ -40,13 +36,18 @@ module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
 
   try {
-    // thread_idとthread_nameをリクエストボディから取得
-    const thread_id: number = req.body.thread_id;
-    const thread_name: string = req.body.thread_name;
-
-    // threadのテーブルにスレッド情報を登録
-    const threadInfo: ThreadInfo = { thread_id, thread_name };
-    await registerThread(threadInfo);
+    // // thread_idとthread_nameをリクエストボディから取得
+    // const id: number = req.body.id;
+    // const img_url: string = req.body.img_url;
+    // const reply_id: number = req.body.reply_id;
+    // const thread_id: number = req.body.thread_id;
+    const id: number = Math.floor(Math.random() * 1000); // 0から999の間でランダムなidを生成
+    const img_url: string = 'https://example.com/image.jpg'; // 仮の画像URL
+    const reply_id: number = Math.floor(Math.random() * 1000); // 0から999の間でランダムなreply_idを生成
+    const thread_id: number = Math.floor(Math.random() * 1000); // 0から999の間でランダムなthread_idを生成
+    // megaのテーブルにスレッド情報を登録
+    const megaInfo: MegaInfo = { id, img_url,reply_id , thread_id};
+    await registerThread(megaInfo);
 
     // 入力完了を出力
     console.log('入力が完了しました。');
